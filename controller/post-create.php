3473d36c04be5b54517b11a396346fccb47d535d
QuangHuy1416 <overtimezero14@gmail.com>
2023-10-26 17:08:58 +0700

    26/10/2023 update 2

--- controller/post-create.php
+++ controller/post-create.php
@@ -4,11 +4,24 @@ $heading = "Create post";
 $method = $_SERVER['REQUEST_METHOD'] ;
 if( $method === "POST")
 {
-    $dbo->query("insert into post(post,user_id)values(:post,:user_id)",[
-        'post' => $_POST['post'],
-        'user_id' => 1
-    ]);
+    $err = [];
+    if(strlen( $_POST['post']) === 0 )
+    {
+        $err['post'] = "A post is required.";
+    }
+    else if(strlen( $_POST['post']) > 1000 )
+    {
+        $err['post'] = "A post can not more than 1000 character.";
+    }
+    if(empty($err))
+    {
+        $dbo->query("insert into post(post,user_id)values(:post,:user_id)",[
+            'post' => $_POST['post'],
+            'user_id' => 1
+        ]);
+        require_once "post.php";
+    }
+    else{
+        require_once "../view/post-create.view.php";
+    }
 }
-//$posts = $db->query("select * from post")->get();
-//require "../view/posts.view.php";
-require_once "post.php";
